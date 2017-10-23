a48818a63f5ab37e4a11768d9e1dca212cd72e0d
Ryan Zell <rzell@EUCLID2003.local>
2017-10-23 11:23:55 -0400

    Initial Setup

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,43 @@
 import React, { Component } from 'react';
+import $ from 'jquery';
+
+import style from '../../style/style.scss';
+
+import TweenMax from "gsap";
+import SplitText from "gsap/SplitText";
+
+function typingAnimation(text, timeline){
+  text.each(function(){
+    var mySplitText = new SplitText(this, {type:"words,chars"});
+    var chars = mySplitText.chars;
+    timeline.staggerFrom(chars,0.01, {opacity:0, ease:Power1.easeIn}, 0.08, "+=0.1");
+    timeline.to(this, 0.6, {delay: 0.8, opacity: 0});
+  });
+
+}
 
 export default class App extends Component {
+  componentDidMount(){
+    var tl =  new TimelineMax();
+    typingAnimation($(".txt"), tl);
+    tl.to($(".centerBox"), 0.6, {delay: 0.2, width: '100%', height: '100%'});
+  }
+
   render() {
     return (
-      <div>React simple starter</div>
+      <div className="body">
+        <div className="centerBox">
+          <div className="scanlines">
+            <div className="boxText" id="textHolder">
+              <h3 className="txt">{"Hello I'm Ryan"}</h3>
+              <h3 className="txt">{"Whats up?"}</h3>
+              <h3 className="txt">{"≧◡≦"}</h3>
+              <h3 id="welcome" className="txt">{"Welcome!"}</h3>
+            </div>
+            {/*<img id="loading" src="../img/bun.gif"/>*/}
+          </div>
+        </div>
+      </div>
     );
   }
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,46 @@
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+function getEntrySources(sources) {
+    if (process.env.NODE_ENV !== 'production') {
+        sources.push('webpack-dev-server/client?http://localhost:8080');
+        sources.push('webpack/hot/only-dev-server');
+    }
+
+    return sources;
+}
+
 module.exports = {
-  entry: [
-    './src/index.js'
-  ],
+  entry: {
+    helloWorld: getEntrySources([
+            './src/index.js'
+        ])
+  },
   output: {
     path: __dirname,
-    publicPath: '/',
-    filename: 'bundle.js'
+    publicPath: 'http://localhost:8080/',
+    filename: 'public/[name].js'
   },
   module: {
-    loaders: [{
-      exclude: /node_modules/,
-      loader: 'babel',
-      query: {
-        presets: ['react', 'es2015', 'stage-1']
-      }
-    }]
+    loaders: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel',
+                query: {
+                    presets: ['react', 'es2015', 'stage-1']
+                }
+            },
+            {
+                test: /\.scss$/,
+                loader: ExtractTextPlugin.extract('css!sass')
+            }
+        ]
   },
+  plugins: [
+        new ExtractTextPlugin('public/main.css', {
+            allChunks: true
+        })
+    ],
   resolve: {
     extensions: ['', '.js', '.jsx']
   },
